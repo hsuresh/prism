0519053b711333c06085eea8853bf66625c1d04f
V Sreekanth <sreeix@gmail.com>
2011-05-17 18:17:38 +0530

    Debugging

--- lib/prism/microformat/hresume.rb
+++ lib/prism/microformat/hresume.rb
@@ -20,6 +20,7 @@ module Prism
       def name
         first_name = get_content('//div[contains(@class, "profile-header")]//span[contains(@class, "given-name")]', @root)
         last_name  = get_content('//div[contains(@class, "profile-header")]//span[contains(@class, "family-name")]', @root)
+        puts 'ddd'
         puts first_name
         puts last_name
         first_name[0] + " " + last_name[0]
